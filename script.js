b39b47fe4a820d116798d5684946ee23f74343a1
nakarin0pond-cloud <nakarin0pond@gmail.com>
2026-03-07 05:52:50 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const text = [
+"Junior Web Developer",
+"Java Spring MVC Developer",
+"Web Application Developer"
+]
+
+let count = 0
+let index = 0
+let current = ""
+let letter = ""
+
+function type(){
+
+if(count === text.length){
+count = 0
+}
+
+current = text[count]
+
+letter = current.slice(0, ++index)
+
+document.querySelector(".typing").textContent = letter
+
+if(letter.length === current.length){
+
+count++
+index = 0
+
+}
+
+setTimeout(type,100)
+
+}
+
+type()
