ae76a85f09b7483e9c3dd9b80f72e546f1cf90c7
nakarin0pond-cloud <nakarin0pond@gmail.com>
2026-03-07 07:24:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,17 @@
-
-const texts = [
-"Junior Web Developer",
-"Backend Developer",
-"Java Developer"
-]
-
-let i = 0
-let j = 0
-let current = ""
-let isDeleting = false
-
-function type(){
-
-current = texts[i]
-
-if(!isDeleting){
-
-document.getElementById("typing").textContent = current.substring(0,j++)
-if(j > current.length){
-
-isDeleting = true
-setTimeout(type,1000)
-return
-
-}
-
-}else{
-
-document.getElementById("typing").textContent = current.substring(0,j--)
-if(j == 0){
-
-isDeleting = false
-i = (i+1)%texts.length
-
-}
-
-}
-
-setTimeout(type,100)
-
-}
-
-type()
-
-
-
-/* dark mode */
 const themeToggle = document.getElementById("themeToggle");
 
-themeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("light-mode");
+themeToggle.onclick = () => {
 
-    if(document.body.classList.contains("light-mode")){
-        themeToggle.innerHTML = "☀️";
-    }else{
-        themeToggle.innerHTML = "🌙";
-    }
-});
+document.body.classList.toggle("light-mode");
+
+if(document.body.classList.contains("light-mode")){
+themeToggle.innerHTML="☀️";
+}else{
+themeToggle.innerHTML="🌙";
+}
+
+}
+
 const observer = new IntersectionObserver(entries => {
 
 entries.forEach(entry => {
@@ -74,5 +29,3 @@ entry.target.classList.add("show");
 document.querySelectorAll(".hidden").forEach(el=>{
 observer.observe(el);
 });
-
-}
