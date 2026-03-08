4ab1eca4b3dfdbf428d0f2730cea5840ab680712
nakarin0pond-cloud <nakarin0pond@gmail.com>
2026-03-09 06:29:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,73 @@
-document.querySelectorAll("a[href^='#']").forEach(anchor => {
+const text = [
+"Junior Web Developer",
+"Java Developer",
+"Backend Developer",
+"Spring MVC Developer"
+]
 
-anchor.addEventListener("click", function(e){
+let i = 0
+let j = 0
+let current = ""
+let isDeleting = false
+
+function type(){
+
+const element = document.querySelector(".typing")
+
+if(i < text.length){
+
+if(!isDeleting && j <= text[i].length){
+
+current = text[i].substring(0,j++)
+element.innerHTML = current
+
+}
+
+if(isDeleting && j >= 0){
+
+current = text[i].substring(0,j--)
+element.innerHTML = current
+
+}
+
+if(j == text[i].length){
+
+isDeleting = true
+setTimeout(type,1000)
+return
+
+}
+
+if(isDeleting && j == 0){
+
+isDeleting = false
+i++
+
+if(i == text.length){
+
+i = 0
+
+}
+
+}
+
+}
+
+setTimeout(type,80)
+
+}
+
+type()
+
+document.querySelectorAll("nav a").forEach(link=>{
+
+link.addEventListener("click",e=>{
 
 e.preventDefault()
 
-document.querySelector(this.getAttribute("href")).scrollIntoView({
+document
+.querySelector(link.getAttribute("href"))
+.scrollIntoView({
 behavior:"smooth"
 })
 
