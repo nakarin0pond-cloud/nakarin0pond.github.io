ee2dc32099ad45976e119254761aa2dff1a59f03
nakarin0pond-cloud <nakarin0pond@gmail.com>
2026-03-09 06:26:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,13 @@
-const input = document.getElementById("taskInput")
-const addBtn = document.getElementById("addTask")
-const list = document.getElementById("taskList")
+document.querySelectorAll("a[href^='#']").forEach(anchor => {
 
-addBtn.onclick = () => {
+anchor.addEventListener("click", function(e){
 
-const text = input.value.trim()
+e.preventDefault()
 
-if(text === "") return
+document.querySelector(this.getAttribute("href")).scrollIntoView({
+behavior:"smooth"
+})
 
-const li = document.createElement("li")
-li.className = "task"
+})
 
-li.innerHTML = `
-<span>${text}</span>
-<button class="delete">X</button>
-`
-
-li.querySelector(".delete").onclick = () => {
-li.remove()
-}
-
-list.appendChild(li)
-
-input.value = ""
-
-}
-
-
-// sidebar toggle
-
-const sb = document.getElementById("sb")
-const open = document.getElementById("sbOpen")
-const close = document.getElementById("sbClose")
-
-open.onclick = () => {
-sb.classList.remove("closed")
-}
-
-close.onclick = () => {
-sb.classList.add("closed")
-}
+})
